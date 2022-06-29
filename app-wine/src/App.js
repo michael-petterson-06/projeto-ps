5a0cc50bd609af3367331a462e83d8145c49076d
Michael <michaelpetterson06.com.br>
2022-06-29 16:48:33 -0300

    criando rota Home

--- app-wine/src/App.js
+++ app-wine/src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'// import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/Navbar';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <Router>
       <NavBar />
-      
+      <Switch>
+        <Route exact path="/">
+            <Home />
+        </Route>
+       
+      </Switch>
     </Router>
       
   
