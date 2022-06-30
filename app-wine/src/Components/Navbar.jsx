29f98b6af86919483c01c117930746aef5f7af69
Michael <michaelpetterson06.com.br>
2022-06-30 20:57:11 -0300

    estruturando NavBar

--- app-wine/src/Components/Navbar.jsx
+++ app-wine/src/Components/Navbar.jsx
@@ -1,7 +1,39 @@
+import { Link } from "react-router-dom";
+
+
+
+
+// IoSearchCircleOutline
+
+
+import search from "../img/search.svg";
+
 function NavBar() {
-    return (
-        <h1>Essa é minha nav</h1>
-    );
+  return (
+    <nav>
+      
+        <Link to="/">
+          <img src={ search } alt="Costs" />
+        </Link>
+        <ul >
+          <li >
+            <Link to="/">Clube</Link>
+          </li>
+          <li >
+            <Link>Lojas</Link>
+          </li>
+          <li >
+            <Link>Produtores</Link>
+          </li>
+          <li >
+            <Link>Eventos</Link>
+          </li>
+        </ul>
+      
+    </nav>
+  );
 }
 
 export default NavBar;
+
+
