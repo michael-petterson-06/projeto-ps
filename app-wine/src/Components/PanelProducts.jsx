9f28567c01dd899847f128e5947b68622e639e87
Michael <michaelpetterson06.com.br>
2022-06-30 12:34:13 -0300

    finalizando estilização do lado direito da tela principal

--- app-wine/src/Components/PanelProducts.jsx
+++ app-wine/src/Components/PanelProducts.jsx
@@ -9,7 +9,13 @@ function PanelProducts(props) {
             priceMember, priceNonMember, rating, region,
            size, sommelierComment, type } = props.products
 
-    
+      
+            
+            const str = priceMember.toString();
+            const splitted = str.split('.');
+            const intPriceMember = parseInt(splitted[0]);
+            const decimalPriceMember = parseInt(splitted[1] || 0);
+            
     return (
         <div className={ styles.cardProduct }>
           <div className={styles.cardInfoProduct} >
@@ -17,13 +23,23 @@ function PanelProducts(props) {
             <img  className={styles.formatImage} src={ image } alt="Editar" />
             <div>{ name }</div>
             <div className={ styles.containerPrice }>
-              <p className={ styles.price }>{ price }</p>
+              <span className={ styles.price }>{ price }</span>
               <span className={ styles.discount }>{` ${ discount }% OFF`}</span>
             </div>
-            
-            <div></div>
-            <div>{ priceMember}</div>
-            <div>{priceNonMember}</div>
+            <div className={ styles.containerSocioWine }>
+              <span className={ styles.SocioWine }>SÓCIO WINE</span>
+              <span className={ styles.real }>R$</span>
+              <span className={ styles.intPriceMember }>{ intPriceMember}</span>
+              <span className={ styles.decimalPriceMember }>{`,${ decimalPriceMember }`} </span>
+            </div>
+            <div className={styles.containerNoSocio}>
+              <span>NÃO SÓCIO</span>
+              <span>{` R$${ priceNonMember }`}</span>
+              <span></span>
+            </div>
+            {/* <div className={styles.blackWine}>
+              BLACK WINE
+            </div> */}
             {/* <div>{avaliations}</div>
             <div>{classification}</div>
             <div>{country}</div>
