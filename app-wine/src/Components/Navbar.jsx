2770b2128f9131e51c44b52b4a29b3be1d5f2b29
Michael <michaelpetterson06.com.br>
2022-06-30 23:49:34 -0300

    estilizando nav

--- app-wine/src/Components/Navbar.jsx
+++ app-wine/src/Components/Navbar.jsx
@@ -2,34 +2,45 @@ import { Link } from "react-router-dom";
 
 
 
+import styles from './Styles/Navbar.module.css';
+import logo from '../img/logo.jpeg';
+import user from '../img/user.jpeg';
+import search from "../img/search.jpeg";
+import carrinho from "../img/carrinho.jpeg";
 
-// IoSearchCircleOutline
-
-
-import search from "../img/search.svg";
 
 function NavBar() {
   return (
-    <nav>
-      
-        <Link to="/">
-          <img src={ search } alt="Costs" />
-        </Link>
-        <ul >
-          <li >
-            <Link to="/">Clube</Link>
-          </li>
-          <li >
-            <Link>Lojas</Link>
-          </li>
-          <li >
-            <Link>Produtores</Link>
-          </li>
-          <li >
-            <Link>Eventos</Link>
-          </li>
-        </ul>
-      
+    <nav className={ styles.navBar }>
+        <div className={styles.containerLinks}>
+            <div className={styles.linkLogo}>
+                <Link  to="/">
+                    <img src={ logo } alt="Costs" />
+                </Link>
+            </div>
+            <ul>
+                <li >
+                    <Link to="/">Clube</Link>
+                </li>
+                <li >
+                    <Link>Lojas</Link>
+                </li>
+                <li >
+                    <Link>Produtores</Link>
+                </li>
+                <li >
+                    <Link>Eventos</Link>
+                </li>
+            </ul>
+        </div>
+        <div className={styles.containerImagens}>
+            <img  src={ search } alt="Foto lupinha" />
+            <img  src={ user } alt="Foto user" />
+            <div className={styles.carrinho}>
+                <img  src={ carrinho } alt="Foto sacolinha" />
+                <div>0</div>
+            </div>
+        </div>
     </nav>
   );
 }
